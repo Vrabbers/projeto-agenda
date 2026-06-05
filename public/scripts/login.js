0b724b83ada245cc6e51a8464a771dc980a9dd67
Vrabbers <25323861+Vrabbers@users.noreply.github.com>
2026-06-05 15:16:33 -0300

    pagina login :)

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,4 +1,26 @@
-document.querySelectorAll("a.troca").forEach(
-    a => a.addEventListener("click", () => {
-        document.querySelectorAll(".trocado").forEach(e => e.classList.toggle("display-none"));
-    }));
+const loginForm = document.querySelector("#login-form");
+const registrarForm = document.querySelector("#registrar-form");
+const avisoErro = document.querySelector("#aviso-erro");
+const params = new URLSearchParams(window.location.search);
+
+
+function doHash() {
+    avisoErro.classList.add("display-none");
+    if (window.location.hash === "#registrar") {
+        loginForm.classList.add("display-none");
+        registrarForm.classList.remove("display-none")
+    } else {
+        loginForm.classList.remove("display-none");
+        registrarForm.classList.add("display-none");
+    }
+}
+
+doHash();
+
+const err = params.get("err");
+if (err) {
+    avisoErro.textContent = err;
+    avisoErro.classList.remove("display-none");
+}
+
+window.addEventListener("hashchange", doHash);
