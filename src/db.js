01773f86d09d2f8633ad672df661f930b60b04fd
Vrabbers <25323861+Vrabbers@users.noreply.github.com>
2026-06-08 22:42:35 -0300

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,17 +1,13 @@
 import 'dotenv/config';
 import mysql2 from "mysql2/promise";
 
-function createDbPool() {
-    return mysql2.createPool(process.DB_URL || {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE,
-    });
-}
-
-export const db = createDbPool();
+export const db = mysql2.createPool(process.DB_URL || {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+});
 
 await db.query("SELECT 1");
 console.log("query ok");
