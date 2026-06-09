cbfad24247e925d73487f15a2b277809095fbb06
Vrabbers <25323861+Vrabbers@users.noreply.github.com>
2026-06-08 22:44:26 -0300

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import mysql2 from "mysql2/promise";
 
-export const db = mysql2.createPool(process.DB_URL || {
+export const db = mysql2.createPool(process.env.DB_URL || {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
