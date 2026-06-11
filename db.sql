4a1e1ed9c5ad8f36c9925a0dbe09e5d00064be94
Vrabbers <25323861+Vrabbers@users.noreply.github.com>
2026-06-10 23:39:35 -0300

    esquema BD

--- db.sql
+++ db.sql
@@ -1,5 +1,34 @@
 CREATE TABLE IF NOT EXISTS usuario (
     id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(40) NOT NULL UNIQUE,
-    senha VARCHAR(255) NOT NULL
+    senha VARCHAR(255) NOT NULL,
+    data_criado DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS evento (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(60) NOT NULL,
+    usuario_id INT NOT NULL REFERENCES usuario(id) ON DELETE CASCADE,
+    dias_da_semana TINYINT NOT NULL,
+    hora_inicio TIME NULL,
+    hora_fim TIME NULL,
+    data_inicio DATE NULL,
+    granularidade ENUM ('hora', 'dia') NOT NULL,
+    data_criado DATETIME DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE (nome, usuario_id)
+);
+
+CREATE TABLE IF NOT EXISTS participante (
+    usuario INT REFERENCES usuario(id) ON DELETE CASCADE,
+    evento INT REFERENCES evento(id) ON DELETE CASCADE,
+    PRIMARY KEY(usuario, evento)
+);
+
+CREATE TABLE IF NOT EXISTS participante_horario_possivel (
+    usuario INT NOT NULL REFERENCES usuario(id) ON DELETE CASCADE,
+    evento INT NOT NULL REFERENCES evento(id) ON DELETE CASCADE,
+    data DATE NOT NULL,
+    hora TIME NULL,
+    comentario TEXT NULL,
+    PRIMARY KEY (usuario, evento, hora)
 );
