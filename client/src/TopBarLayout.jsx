a6baf4be8ec5e75d788c67de29e7a93e6ee6e0df
Vrabbers <25323861+Vrabbers@users.noreply.github.com>
2026-06-12 00:05:59 -0300

    comeca pagina inicial

--- client/src/TopBarLayout.jsx
+++ client/src/TopBarLayout.jsx
@@ -1,4 +1,5 @@
-import { Link, NavLink, Outlet } from "react-router-dom";
+import "./styles/top-bar.css";
+import { Link, Outlet } from "react-router-dom";
 import { isAuth, isNotAuth, useAuth } from "./auth-context";
 
 const notAuthPage = <>
@@ -10,15 +11,13 @@ export default function TopBarLayout() {
     const [auth, reauth] = useAuth();
     
     const clickLogout = async () => {
-        const res = await fetch("/api/logout");
-        if (res.ok) {
-            await reauth();
-        }
+        await fetch("/api/logout");
+        await reauth();
     };
     
     return <>
         <header>
-            <h1><Link to="/" className="main-a"><img src="/img/event_available.svg" width="28" /> Agenda Compartilhada</Link></h1>
+            <h1><Link to="/" className="main-a"><img src="/img/event_available.svg" width="28" /> <span className="main-a-texto">Agenda Compartilhada</span></Link></h1>
             <div className="user-box">
                 {
                     isAuth(auth) &&  (
@@ -33,18 +32,8 @@ export default function TopBarLayout() {
                     isNotAuth(auth) && <Link to="/login" className="botao">Fazer login</Link>
                 }
             </div>
-            {
-                isAuth(auth) && (
-                    <nav className="auth">
-                        <ul>
-                            <li><NavLink to="/ver-eventos">Ver eventos</NavLink></li>
-                            <li><NavLink to="/criar-evento">Criar evento</NavLink></li>
-                        </ul>
-                    </nav>
-                )
-            }
         </header>
-        <main class="normal-main">
+        <main className="normal-main">
             {
                 (isAuth(auth) && <Outlet />) || (isNotAuth(auth) && notAuthPage) 
             }
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,10 +1,14 @@
-import { } from "react-router-dom";
+import {} from "react-router-dom";
 import { isAuth, useAuth } from "../auth-context";
 
 export default function HomePage() {
     const [auth] = useAuth();
 
-    if (isAuth(auth)) {
-        return JSON.stringify(auth);
-    } 
+    if (!isAuth(auth)) return null;
+
+    return (
+        <>
+            <h2>Eventos que você criou</h2>
+        </>
+    );
 }
